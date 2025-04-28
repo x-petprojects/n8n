104f9be2ce3d7ebf96594d6f2b711bf48b3774a8
sergey.petrenko <serge.petrenko@gmail.com>
2025-04-28 19:49:33 +0300

    feat(credentials): Update Yandex API credential properties and URLs for improved authentication

--- packages/nodes-base/credentials/YandexApi.credentials.ts
+++ packages/nodes-base/credentials/YandexApi.credentials.ts
@@ -1,118 +1,113 @@
 import type { ICredentialType, INodeProperties } from 'n8n-workflow';
 
 export class YandexApi implements ICredentialType {
-	name = 'YandexApi';
+    name = 'yandexApi';
 
-	displayName = 'Yandex API';
+    displayName = 'Yandex API';
 
-	documentationUrl = 'httpRequest';
+    documentationUrl = 'https://yandex.com/dev/id/doc/dg/oauth/';
 
-	genericAuth = true;
+    genericAuth = true;
 
-	properties: INodeProperties[] = [
-		{
-			displayName: 'Grant Type',
-			name: 'grantType',
-			type: 'options',
-			options: [
-				{
-					name: 'Authorization Code',
-					value: 'authorizationCode',
-				},
-				{
-					name: 'Client Credentials',
-					value: 'clientCredentials',
-				},
-				{
-					name: 'PKCE',
-					value: 'pkce',
-				},
-			],
-			default: 'authorizationCode',
-		},
-		{
-			displayName: 'Authorization URL',
-			name: 'authUrl',
-			type: 'string',
-			displayOptions: {
-				show: {
-					grantType: ['authorizationCode', 'pkce'],
-				},
-			},
-			default: '',
-			required: true,
-		},
-		{
-			displayName: 'Access Token URL',
-			name: 'accessTokenUrl',
-			type: 'string',
-			default: '',
-			required: true,
-		},
-		{
-			displayName: 'Client ID',
-			name: 'clientId',
-			type: 'string',
-			default: '',
-			required: true,
-		},
-		{
-			displayName: 'Client Secret',
-			name: 'clientSecret',
-			type: 'string',
-			typeOptions: {
-				password: true,
-			},
-			default: '',
-			required: true,
-		},
-		// WARNING: if you are extending from this credentials and allow user to set their own scopes
-		// you HAVE TO add it to GENERIC_OAUTH2_CREDENTIALS_WITH_EDITABLE_SCOPE in packages/cli/src/constants.ts
-		// track any updates to this behavior in N8N-7424
-		{
-			displayName: 'Scope',
-			name: 'scope',
-			type: 'string',
-			default: '',
-		},
-		{
-			displayName: 'Auth URI Query Parameters',
-			name: 'authQueryParameters',
-			type: 'string',
-			displayOptions: {
-				show: {
-					grantType: ['authorizationCode', 'pkce'],
-				},
-			},
-			default: '',
-			description:
-				'For some services additional query parameters have to be set which can be defined here',
-			placeholder: 'access_type=offline',
-		},
-		{
-			displayName: 'Authentication',
-			name: 'authentication',
-			type: 'options',
-			options: [
-				{
-					name: 'Body',
-					value: 'body',
-					description: 'Send credentials in body',
-				},
-				{
-					name: 'Header',
-					value: 'header',
-					description: 'Send credentials as Basic Auth header',
-				},
-			],
-			default: 'header',
-		},
-		{
-			displayName: 'Ignore SSL Issues (Insecure)',
-			name: 'ignoreSSLIssues',
-			type: 'boolean',
-			default: false,
-			doNotInherit: true,
-		},
-	];
+    properties: INodeProperties[] = [
+        {
+            displayName: 'Grant Type',
+            name: 'grantType',
+            type: 'options',
+            options: [
+                {
+                    name: 'Authorization Code',
+                    value: 'authorizationCode',
+                },
+                {
+                    name: 'Client Credentials',
+                    value: 'clientCredentials',
+                },
+            ],
+            default: 'authorizationCode',
+        },
+        {
+            displayName: 'Authorization URL',
+            name: 'authUrl',
+            type: 'string',
+            displayOptions: {
+                show: {
+                    grantType: ['authorizationCode'],
+                },
+            },
+            default: 'https://oauth.yandex.com/authorize',
+            required: true,
+        },
+        {
+            displayName: 'Access Token URL',
+            name: 'accessTokenUrl',
+            type: 'string',
+            default: 'https://oauth.yandex.com/token',
+            required: true,
+        },
+        {
+            displayName: 'Client ID',
+            name: 'clientId',
+            type: 'string',
+            default: '',
+            required: true,
+        },
+        {
+            displayName: 'Client Secret',
+            name: 'clientSecret',
+            type: 'string',
+            typeOptions: {
+                password: true,
+            },
+            default: '',
+            required: true,
+        },
+        {
+            displayName: 'Scope',
+            name: 'scope',
+            type: 'string',
+            default: '',
+            description:
+                'Comma-separated list of permissions requested by the application. For example: "login:email".',
+        },
+        {
+            displayName: 'Auth URI Query Parameters',
+            name: 'authQueryParameters',
+            type: 'string',
+            displayOptions: {
+                show: {
+                    grantType: ['authorizationCode'],
+                },
+            },
+            default: '',
+            description:
+                'Additional query parameters for the authorization URL, if required.',
+            placeholder: 'force_confirm=yes',
+        },
+        {
+            displayName: 'Authentication',
+            name: 'authentication',
+            type: 'options',
+            options: [
+                {
+                    name: 'Body',
+                    value: 'body',
+                    description: 'Send credentials in the request body',
+                },
+                {
+                    name: 'Header',
+                    value: 'header',
+                    description: 'Send credentials as Basic Auth header',
+                },
+            ],
+            default: 'body',
+        },
+        {
+            displayName: 'Ignore SSL Issues (Insecure)',
+            name: 'ignoreSSLIssues',
+            type: 'boolean',
+            default: false,
+            doNotInherit: true,
+        },
+    ];
 }
